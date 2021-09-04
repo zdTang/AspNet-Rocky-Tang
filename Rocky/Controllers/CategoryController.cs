ab12b8646d298cf06ab5326ea1e1cd803614c7a8
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-09-04 11:49:58 -0400

    can read category data

---
+++ Rocky/Controllers/CategoryController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;                                          //  Dependency Injection
+        }
+        
+        public IActionResult Index()
+        {
+
+            IEnumerable<Category> objList = _db.Category;     //  Grab a collection from DB
+            
+            
+            return View(objList);
+            //var content = new ContentResult();
+            //content.Content = "hello";
+            //return content;
+        }
+
+        // Create a new Category
+        public IActionResult Create()
+        {
+
+                 //  Grab a collection from DB
+
+
+            return View();
+
+        }
+    }
+}
---
+++ Rocky/README.txt
@@ -0,0 +1,7 @@
+﻿##  create Category model
+    0, Add connection string to "appsettings.json"
+    1, create Category class   (Pay attention to Attribute, such as [Key])
+    2, create ApplicationDbContext class (Install EF package and inherit DbContext base class)
+    3, DI:  Setup Startup.cs, inject DBcontext into Service container
+    4, Using PM to do the migration.   a, add-migration, b, update-database  (Maybe need install EF TOOL package)
+       Not very sure how to tune those fields....
